ba3c553157845c22dd5d7cc588f0be7aa9e8a982
fruitsdrink <fruitsdrink@126.com>
2024-11-26 17:18:23 +0800

    feat: add reanimated-carousel screenii

--- src/app/animation/reanimated-carousel.tsx
+++ src/app/animation/reanimated-carousel.tsx
@@ -2,7 +2,7 @@ import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { Stack } from "expo-router";
 
-export default function ReanimatedCarousel() {
+export default function ReanimatedCarouselScreen() {
   return (
     <View style={styles.container}>
       <Stack.Screen
